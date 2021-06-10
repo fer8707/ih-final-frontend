2451fa7eda8ff01d6820dce5f4cff82b83382a5d
Fernando Tavera <fer8707@gmail.com>
2021-06-09 23:56:15 -0500

    Update with styles listado

--- src/components/trabajos/ListadoTrabajos.js
+++ src/components/trabajos/ListadoTrabajos.js
@@ -1,43 +1,40 @@
 import React, { useContext, useEffect, useState } from "react";
 import clienteAxios from "../../config/axios";
-import axios from "axios";
-import { loader } from '@googlemaps/js-api-loader'
+import moment from "moment";
+import { loader } from "@googlemaps/js-api-loader";
 
 import TrabajoContext from "./../../context/trabajos/TrabajoContext";
 import AuthContext from "./../../context/autenticacion/AuthContext";
 
 export default function ListadoTrabajos() {
-    
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
   const [titulo, setTitulo] = useState("");
   const [descripcion, setDescripcion] = useState("");
   const [duracion, setDuracion] = useState("");
   const [requisitos, setRequsitos] = useState("");
   const [ubicacion, setUbicacion] = useState("");
-// --------------------------GoogleMaps--------------
-//     const loader = new Loader({
-//     apiKey: "AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik",
-//     version: "weekly",
-//     libraries: ["places"]
-//     });
+  // --------------------------GoogleMaps--------------
+  //     const loader = new Loader({
+  //     apiKey: "AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik",
+  //     version: "weekly",
+  //     libraries: ["places"]
+  //     });
 
-//     const mapOptions = {
-//     center: {
-//         lat: 0,
-//         lng: 0
-//     },
-//     zoom: 4
-//     };
-// // Callback
-// loader.loadCallback(e => {
-//     if (e) {
-//       console.log(e);
-//     } else {
-//       new google.maps.Map(document.getElementById("map"), mapOptions);
-//     }
-//   });
-
-
+  //     const mapOptions = {
+  //     center: {
+  //         lat: 0,
+  //         lng: 0
+  //     },
+  //     zoom: 4
+  //     };
+  // // Callback
+  // loader.loadCallback(e => {
+  //     if (e) {
+  //       console.log(e);
+  //     } else {
+  //       new google.maps.Map(document.getElementById("map"), mapOptions);
+  //     }
+  //   });
 
   // EXTRAER LOS VALORES DEL CONTEXT (ESTADO GLOBAL)
   const trabajoContext = useContext(TrabajoContext);
@@ -65,7 +62,6 @@ export default function ListadoTrabajos() {
     e.preventDefault();
     crearTrabajo();
   };
-  
 
   const postNuevoFormulario = async () => {
     const data = {
@@ -73,7 +69,7 @@ export default function ListadoTrabajos() {
       descripcion: descripcion,
       duracion: duracion,
       requisitos: requisitos,
-      ubicacion: ubicacion
+      ubicacion: ubicacion,
     };
 
     await clienteAxios.post("http://localhost:4000/api/trabajos", data);
@@ -85,8 +81,7 @@ export default function ListadoTrabajos() {
     <div>
       {mostrarFormulario ? (
         <div className="contenedor-formulario">
-          <h1>Crea un nuevo trabajo</h1>
-          <span> Título del trabajo: </span>
+          <h1> Crea un nuevo trabajo </h1> <span> Título del trabajo: </span>
           <input
             className="input-formulario-trabajo"
             onChange={(event) => {
@@ -122,66 +117,84 @@ export default function ListadoTrabajos() {
             }}
           />
           <button
-            className="btn btn-lg btn-primary" type="button"
+            className="btn btn-lg btn-primary"
+            type="button"
             onClick={() => {
               postNuevoFormulario();
             }}
           >
-            {" "}
-            enviar{" "}
+            enviar
           </button>
         </div>
       ) : (
-        <div className= "listado">
-            <div className= "fragmentos">
-            <>
-            {" "}
-            <h1>Listado de trabajos</h1>
+        <div className="listado-padre">
+          <div className="listado-cards">
+            <div className="grupo-boton">
+              <button
+                className="floating-button"
+                type="button"
+                onClick={async () => {
+                  console.log("-----");
+                  setMostrarFormulario(!mostrarFormulario);
+                  await obtenerTrabajos();
+                }}
+              >
+                {mostrarFormulario ? "Ver listado" : "Crear trabajo"}
+              </button>
+              <button
+                className="floating-button"
+                type="button"
+                onClick={(e) => {
+                  clickLogout(e);
+                }}
+              >
+                Cerrar sesión
+              </button>
+            </div>
             {trabajos.length === 0
-                ? "No hay trabajos listados"
-                : trabajos.map((e) => {
-                    return (
-                    <p key={e._id}>
-                        {e.creado}
-                        <br />
+              ? "No hay trabajos listados"
+              : trabajos.map((e) => {
+                  return (
+                    <div className="card-trabajo" key={e._id}>
+                      <div className="card-header">
+                        <span className="tag-duracion">{e.duracion}</span>
+                        <div className="fecha-info-contenedor">
+                          <span className="fecha-label-card">Fecha:</span>
+                          <p className="parrafo-card">
+                            {moment(e.creado).format("MMM Do YY")}
+                          </p>
+                        </div>
+                      </div>
+                      <p className="parrafo-card">
+                        <span className="titilo-label-card">Nombre:</span>
                         {e.titulo}
-                        <br />
+                      </p>
+
+                      <p className="parrafo-card">
+                        <span className="titilo-label-card">Descripcion:</span>
                         {e.descripcion}
-                        <br />
-                        {e.duracion}
-                        <br />
+                      </p>
+                      <p className="parrafo-card">
+                        <span className="titilo-label-card">Requisitos:</span>
                         {e.requisitos}
-                        <br />
-                        <a href={e.ubicacion}>ubicacion</a>
-                    </p>
-                    );
-                })}{" "}
-            </>
-            </div>
-            <div className="fragmentos">
-            <>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12655.147669967391!2d-99.14132440749697!3d19.43840360682754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1623280825806!5m2!1ses!2smx"loading="lazy"></iframe>
-            </>
-            </div>
+                      </p>
+                      <button className="button-ubicacion">
+                        <a href={e.ubicacion}> Ver Ubicación </a>
+                      </button>
+                    </div>
+                  );
+                })}
+          </div>
+          <div className="mapa-contenedor">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12655.147669967391!2d-99.14132440749697!3d19.43840360682754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1623280825806!5m2!1ses!2smx"
+              // width="100vw"
+              allowfullscreen
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
       )}
-
-      <button className="btn btn-lg btn-primary" type="button"
-        onClick={(e) => {
-          clickLogout(e);
-        }}
-      >
-        Cerrar sesión
-      </button>
-      <button className="btn btn-lg btn-primary" type="button"
-        onClick={async () => {
-          console.log("-----");
-          setMostrarFormulario(!mostrarFormulario);
-          await obtenerTrabajos();
-        }}
-      >
-        {mostrarFormulario ? "Ver listado" : "Crear trabajo"}
-      </button>
     </div>
   );
 }
